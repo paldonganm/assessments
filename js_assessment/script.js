5b998dd2b812fcfb6d8a3938d6d0beb1a5693b21
paldonganm <127816153+paldonganm@users.noreply.github.com>
2023-08-07 10:23:55 +0530

    Add files via upload

---
+++ js_assessment/script.js
@@ -0,0 +1,7 @@
+function validateForm(){
+    const email = document.getElementById('email');
+    const emailErr = document.getElementById('emailErr');
+    const RegExp =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+}
